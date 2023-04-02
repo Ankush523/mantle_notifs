15306b2bf38fc53582d69cb36bb5cfc866ebee71
Ankush523 <ankush.ad050203@gmail.com>
2023-04-02 23:17:30 +0530

    confirmed txn details returned

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,44 @@
-const ethers = require("ethers");
-const address = "0x816fe884C2D2137C4F210E6a1d925583fa4A917d";
-const mantleurl = "wss://ws.testnet.mantle.xyz";
-const polygonurl = "wss://polygon-mumbai.g.alchemy.com/v2/B_5czQpQeXc_6pZlC-wDa_-QD1xhTI86";
-const init = function () {
+//for pending transactions
+
+// const ethers = require("ethers");
+// const address = "0x816fe884C2D2137C4F210E6a1d925583fa4A917d";
+// const mantleurl = "wss://ws.testnet.mantle.xyz";
+// const polygonurl = "wss://polygon-mumbai.g.alchemy.com/v2/B_5czQpQeXc_6pZlC-wDa_-QD1xhTI86";
+// const init = function () {
   
-  const customWsProvider = new ethers.WebSocketProvider(mantleurl);
+//   const customWsProvider = new ethers.WebSocketProvider(polygonurl);
 
-  customWsProvider.on("pending", (tx : string) => {
-    customWsProvider.getTransaction(tx).then(function (transaction : any) {
-      if (transaction.from == address || transaction.to == address) {
-        console.log(transaction);
-      }
-    });
-  });
+//   customWsProvider.on("pending", (tx : string) => {
+//     customWsProvider.getTransaction(tx).then(function (transaction : any) {
+//       if (transaction.from == address || transaction.to == address) {
+//         console.log(transaction);
+//       }
+//     });
+//   });
+// };
+// init();
 
-  // customWsProvider.on("block", (blockNumber : number) => {
-  //   customWsProvider.getBlock(blockNumber).then(function (block : any) {
-  //     console.log(block);
-  //   });
-  // });
 
-  customWsProvider.websocket.on("error", async () => {
-    console.log(`Unable to connect to ${mantleurl} retrying in 3s...`);
-    setTimeout(init, 3000);
-  });
+//for confirmed transactions
 
-  customWsProvider.websocket.on("close", async (code : number) => {
-    console.log(
-      `Connection lost with code ${code}! Attempting reconnect in 3s...`
-    );
-    customWsProvider._websocket.terminate();
-    setTimeout(init, 3000);
-  });
-};
+import { ethers } from 'ethers';
+const provider = new ethers.WebSocketProvider('wss://polygon-mumbai.g.alchemy.com/v2/B_5czQpQeXc_6pZlC-wDa_-QD1xhTI86');
 
-init();
+async function getConfirmedTransactionDetails(txHash: string) {
+  const tx = await provider.getTransaction(txHash);
+  if (tx && tx.blockNumber) {
+    if(tx.from == "0x816fe884C2D2137C4F210E6a1d925583fa4A917d" || tx.to == "0x816fe884C2D2137C4F210E6a1d925583fa4A917d")
+    {
+      console.log(tx);
+    }
+  }
+}
+
+provider.on('block', async (blockNumber) => {
+  const block = await provider.getBlock(blockNumber, true);
+  if (block && block.transactions) {
+    for (const tx of block.transactions) {
+      await getConfirmedTransactionDetails(tx);
+    }
+  }
+});
